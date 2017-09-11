dc482dd05a93c7e577ba08a2e840a1d5631fe2de
unknown <yashvishah01@gmail.com>
2017-09-11 22:46:31 +0530

    responsive

--- conn.php
+++ conn.php
@@ -1,14 +1,7 @@
  <?php
 $host="localhost";
-<<<<<<< HEAD
  $username="root";    			// id2784862_root specify the sever details for mysql
  $password="";              //pass@123
  $database="iadmin";              //id2784862_iadmin
 $conn = mysqli_connect($host,$username,$password, $database);
-=======
- $username="id2784862_root";    			// specify the sever details for mysql
- $password="pass@123";
- $database="id2784862_iadmin";
-$conn= mysqli_connect($host,$username,$password, $database);
->>>>>>> 787398d2b2bf7ff3492ded2daf5e20bb8525d7f7
 ?>
